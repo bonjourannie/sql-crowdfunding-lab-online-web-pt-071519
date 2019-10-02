8b79cd7f61611855c6fad0b02386644956ea6551
Annie Truex <artruex@gmail.com>
2019-10-02 21:38:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,14 @@
 INSERT INTO users (id, name, age) VALUES (1, 'Annie', 25), (2, 'Dan', 30), (3, 'Gracie', 25), (4, 'Theresa', 29), (5, 'Pete', 64), (6, 'Adam', 25), (7, 'Christopher', 65), (8, 'Gretchen', 65), (9, 'Stacy', 32), (10, 'Dave', 29), (11, 'Mel', 30), (12, 'Arian', 34), (13, 'Alex', 34), (14, 'Jan', 64), (15, 'Rose', 31), (16, 'Jason', 40), (17, 'Mike', 29), (18, 'Grendel', 9), (19, 'Skip', 9), (20, 'Scruffy', 9);
 
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
-(1, 'i am too short', 'surgery', 10000, 'birth', 'later'), (2, 'fix global warming', 'science', 45, 'October', 'too_late'), (3, 'need more dogs', 'animals', 600, 'January', 'May'), (4, 'too many dogs', 'animals', 1200, 'May', 'ASAP'), (5, 'i miss my dogs', 'animals', 2400, 'June', 'today'), (6, 'making a movie', 'entertainment', 1000000000, 'Feb_12', 'Feb 29'), (7, 'feeling poor', 'charity', 1000000000, 'April_15', 'April_31') (8, 'more dogs', 'animals', 500, 'now', 'now'), (9, 'out of ideas', 'dunno', 4, 'tomorrow', 'yesterday'), (10, 'walking', 'exercise', 73, 'monday', 'friday')
+(1, 'i am too short', 'surgery', 10000, 'birth', 'later'), 
+(2, 'fix global warming', 'science', 45, 'October', 'too_late'), 
+(3, 'need more dogs', 'animals', 600, 'January', 'May'), 
+(4, 'too many dogs', 'animals', 1200, 'May', 'ASAP'), 
+(5, 'i miss my dogs', 'animals', 2400, 'June', 'today'), 
+(6, 'making a movie', 'entertainment', 1000000000, 'Feb_12', 'Feb 29'), 
+(7, 'feeling poor', 'charity', 1000000000, 'April_15', 'April_31'),
+(8, 'more dogs', 'animals', 500, 'now', 'now'), 
+(9, 'out of ideas', 'dunno', 4, 'tomorrow', 'yesterday'), 
+(10, 'walking', 'exercise', 73, 'monday', 'friday')
 ;
